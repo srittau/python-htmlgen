5d498b6d373bf2304896dab6f3d2eda086beb16e
Sebastian Rittau <srittau@rittau.biz>
2014-03-17 19:58:48 +0100

    add missing import for ShortElement

--- htmlgen/__init__.py
+++ htmlgen/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
 from .block import Division
-from .element import (Element, html_attribute, boolean_html_attribute,
+from .element import (Element, ShortElement,
+                      html_attribute, boolean_html_attribute,
                       int_html_attribute)
 from .generator import (Generator, NullGenerator, ChildGenerator,
                         HTMLChildGenerator, JoinGenerator, HTMLJoinGenerator)
