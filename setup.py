5ea089d150a7e6132670ab92521bf763b1e719bf
Sebastian Rittau <srittau@rittau.biz>
2014-03-17 19:18:55 +0100

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="htmlgen",
-    version="0.1",
+    version="0.2",
     description="HTML 5 Generator",
     author="Sebastian Rittau",
     author_email="srittau@rittau.biz",
