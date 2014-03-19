54d5d2cef00e02c7104b324f3f397880518305ff
Sebastian Rittau <srittau@rittau.biz>
2014-03-19 10:06:36 +0100

    improve Division docstring

--- htmlgen/block.py
+++ htmlgen/block.py
@@ -3,11 +3,17 @@ from htmlgen.element import Element
 
 class Division(Element):
 
-    """A <div> element.
+    """A html division (<div>) element.
 
     <div> elements are block-level elements without semantic meaning. They are
     containers for styling or scripting.
 
+        >>> div = Division()
+        >>> div.id = "my-block"
+        >>> div.add_css_classes("important", "legal")
+        >>> div.append("This is important legal text.")
+
     """
+
     def __init__(self):
         super(Division, self).__init__("div")
