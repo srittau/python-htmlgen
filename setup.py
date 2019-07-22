03da9bd04ae7b28ca9429a5a2e4945b5f3aacf55
Sebastian Rittau <srittau@rittau.biz>
2019-07-22 12:04:11 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=["htmlgen", "test_htmlgen"],
     package_data={"htmlgen": ["*.pyi", "py.typed"]},
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    tests_require=["asserts >= 0.8.0, < 0.9", "typing"],
+    tests_require=["asserts >= 0.8.0, < 0.10", "typing"],
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
